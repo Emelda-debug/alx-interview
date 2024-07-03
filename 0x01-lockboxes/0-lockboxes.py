269c18f5e6e77a13780ecef1a388026b72ceaf0f
Emelda-debug <teacayemmie@gmail.com>
2024-07-03 16:26:48 +0200

    more corrections

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,62 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 method that determines if all the boxes can be opened
 given there are n number of boxes where
 Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes
 """
+
 def canUnlockAll(boxes):
+        """ function to check if all the boxes can be opened"""
         if (type(boxes)) is not list:
                 return False
         elif (len(boxes)) == 0:
                 return False
-        
         for x in range(1, len(boxes) - 1):
                 box_check = False
                 for index in range(len(boxes)):
-                        boxes_checked = x in boxes[index] and x != index
+                        box_check = x in boxes[index] and x != index
                         if box_check:
                                 break
-                        if box_check is False:
-                                return box_check
-                return True                                                      
+                if box_check is False:
+                        return box_check
+        return True
+                        
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """ """
+
+        
+                
+        
+                
+        
+        
+                
+                
+                        
+                        
+                                
+                        
+                                
+                                                                      
