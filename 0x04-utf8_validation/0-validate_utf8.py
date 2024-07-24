eb80eabbd118b621e5ec802c8a6f33148e718fff
emelda-debug <teacayemmie@gmail.com>
2024-07-24 17:49:01 +0400

    tat

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,27 +3,32 @@
 represents a valid UTF-8 encoding"""
 
 
-def count_num_of_leading_set_bits(byte_value):
-  """ function to counts the number of leading set bits (1) in a byte"""
-  num_set_bits = 0
-  mask = 1 << 7
-  while byte_value & mask:
-    num_set_bits += 1
-    mask >>= 1
-  return num_set_bits
 
-def is_valid_utf8(byte_sequence):
-  """Determines if the given byte sequence represents valid UTF-8 encoding."""
-  num_bytes_to_process = 0
-  for byte in byte_sequence:
-    if num_bytes_to_process == 0:
-      num_bytes_to_process = count_num_of_leading_set_bits(byte)
-      if num_bytes_to_process == 0:
-        continue  
-      if num_bytes_to_process == 1 or num_bytes_to_process > 4:
-        return False  
-    else:
-      if not (byte & (1 << 7) and not (byte & (1 << 6))):
-        return False  
-      num_bytes_to_process -= 1
-  return num_bytes_to_process == 0
+def count_num_of_leading_set_bits(num):
+    """returns the number of leading set bits (1)"""
+    num_set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        num_set_bits += 1
+        helper = helper >> 1
+    return num_set_bits
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    bits_count = 0
+    for x in range(len(data)):
+        if bits_count == 0:
+            bits_count = count_num_of_leading_set_bits(data[x])
+            '''1-byte (format: 0xxxxxxx)'''
+            if bits_count == 0:
+                continue
+            """character in UTF-8 can be 1 - 4 bytes long"""
+            if bits_count == 1 or bits_count > 4:
+                return False
+        else:
+            """checks if current byte has correct format"""
+            if not (data[x] & (1 << 7) and not (data[x] & (1 << 6))):
+                return False
+        bits_count -= 1
+    return bits_count == 0
