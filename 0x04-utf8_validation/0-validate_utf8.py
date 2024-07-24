091afc9b70f78239095d7f605dbd2ee05164c54c
emelda-debug <teacayemmie@gmail.com>
2024-07-24 16:46:18 +0400

    tasks

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Method that determines if a given data set
+represents a valid UTF-8 encoding"""
+
+
+def count_num_of_leading_set_bits(byte_value):
+    """ function to counts the number of leading set bits (1) in a byte"""
+    num_set_bits = 0
+    mask = 1 << 7
+    while byte_value & mask:
+        num_set_bits += 1
+        mask >>= 1
+    return num_set_bits
+
+
+def is_valid_utf8(byte_sequence):
+    """Determines if the given byte sequence represents valid UTF-8 encoding."""
+    num_bytes_to_process = 0
+    for byte in byte_sequence:
+        if num_bytes_to_process == 0:
+            num_bytes_to_process = count_num_of_leading_set_bits(byte)
+            if num_bytes_to_process == 0:
+                continue
+            if num_bytes_to_process == 1 or num_bytes_to_process > 4:
+                return False
+        else:
+            if not (byte & (1 << 7) and not (byte & (1 << 6))):
+                return False
+            num_bytes_to_process -= 1
+    return num_bytes_to_process == 0
